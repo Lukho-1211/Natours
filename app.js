fcfcc40f853eabc6b7dd99b14a64b90b604af142
Lukho-1211 <67160564+Lukho-1211@users.noreply.github.com>
2024-09-17 19:02:58 +0200

    upload json data

---
+++ app.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+// app.get('/', (req,res)=>{
+//     res.status(200).json({message:'My business..',me:'That me'});
+// });
+
+// app.post('/',(req,res)=>{
+//     res.status(200).json({mes:'Posting something works...'});
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
+app.listen(port, ()=>{
+    console.log(`Connected to port ${port}`);
+});
