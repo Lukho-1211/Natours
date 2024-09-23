378d536ea215652f17adcd5da73e186545fa9860
Lukho-1211 <67160564+Lukho-1211@users.noreply.github.com>
2024-09-23 14:37:52 +0200

    Advanced filter, Sort, Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,44 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async(req,res)=>{
     try {
-        const tours = await Tour.find();
+        console.log(req.query);
+
+        // BUILD QUERY
+        // 1A) Filtering
+        const queryObj = { ...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el=> delete queryObj[el]);
+
+        //1B) Advanced filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match=> `$${match}`);
+        //console.log(JSON.parse(queryStr));
+        
+        let query = Tour.find(JSON.parse(queryStr));
+        // 2) Sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }else{
+            query = query.sort('-createdAt');
+        }
+        
+        // 3) Feild limiting
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        }else{
+            query = query.select('-__v');
+        }
+        // const query = Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy');
+
+        const tours = await query;
+
+        // EXECUTE QUERY
         res.status(200).json({
             status: 'success',
             result: tours.length,
@@ -14,7 +51,7 @@ exports.getAllTours = async(req,res)=>{
     } catch (err) {
         res.status(404).json({
             success: 'false',
-            message: 'Could not get Tours'
+            message: err
         })
     }
 }
