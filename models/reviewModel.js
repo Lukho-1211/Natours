d85905385428e4f56323ba3589c07474f2c8083b
Lukho-1211 <67160564+Lukho-1211@users.noreply.github.com>
2024-11-06 08:48:15 +0200

    Add average and quantity

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//const Tour = require('./tourModel');
+const Tour = require('./tourModel');
 //const User = require('./userModel');
 
 const reviewSchema = new mongoose.Schema({
@@ -49,7 +49,56 @@ reviewSchema.pre(/^find/, function(next){
     });
     
     next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId){
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId} 
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating'}
+            }
+        }
+    ]);
+    //console.log(stats);
+    if(stats.length > 0){
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuanity: stats[0].nRating,
+            ratingsAvarage: stats[0].avgRating
+        })
+    }else{
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuanity: 0,
+            ratingsAvarage: 4.5
+        })
+    }
+};
+
+reviewSchema.post('save', function(){
+    this.constructor.calcAverageRatings(this.tour);
 })
+
+//this code executes before deleting or updating doc
+    //findByIdAndUpdate also uses findOneAndDate under the scans
+    //findByIdAndDelete also uses findOneAndDelete under the scans
+reviewSchema.pre(/^findOneAnd/, async function(next){
+    this.r = await this.findOne();
+    console.log(this.r);
+    next();
+})
+
+//this code executes after deleting or updating doc
+    //findByIdAndUpdate also uses findOneAndDate under the scans
+    //findByIdAndDelete also uses findOneAndDelete under the scans
+    reviewSchema.post(/^findOneAnd/, async function(){
+    //  await this.findOne(); does not work here, query has already exceduted 
+        await this.r.constructor.calcAverageRatings(this.r.tour)
+    })
+    
 
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
